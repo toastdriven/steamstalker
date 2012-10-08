02cd48f555650219589e96947ec47b62bc969e79
Daniel Lindsley <daniel@toastdriven.com>
2012-10-08 14:02:15 -0500

    Only slightly better error handling.

--- steamstalker/management/commands/gotta_stalk_em_all.py
+++ steamstalker/management/commands/gotta_stalk_em_all.py
@@ -5,4 +5,9 @@ from ...models import SteamProfile
 class Command(NoArgsCommand):
     def handle_noargs(self, *args, **kwargs):
         for profile in SteamProfile.objects.filter(import_me=True):
-            profile.get_latest_data()
+            try:
+                profile.get_latest_data()
+            except Exception as e:
+                # This sucks but lots of things can go wrong & it'd be better to
+                # let as many complete as possible.
+                print "{0} failed: {1}".format(profile.username, e)
